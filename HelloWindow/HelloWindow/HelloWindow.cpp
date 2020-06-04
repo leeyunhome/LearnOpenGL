47788bcc55826cf3aa6ca5e4c855e8e2c0795fd5
이윤호 <neuezeal@gmail.com>
2020-06-04 14:21:51 +0900

    add Element Buffer Objects

--- HelloWindow/HelloWindow/HelloWindow.cpp
+++ HelloWindow/HelloWindow/HelloWindow.cpp
@@ -10,19 +10,32 @@ using namespace std;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-const char* vertexShaderSource = "#version 330 core \
-		layout(location = 0) in vec3 aPos;\
-		void main()\
-	{\
-		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\
-	}";
+//const char* vertexShaderSource = "#version 330 core \n\
+//		layout(location = 0) in vec3 aPos;\
+//		void main()\
+//	{\
+//		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\
+//	}";
+//
+//const char* fragmentShaderSource = "#version 330 core \n\
+//out vec4 FragColor;\
+//void main()\
+//{\
+//FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\
+//		";
 
-const char* fragmentShaderSource = "version 330 core\
-out vec4 FragColor;\
-void main()\
-{\
-FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\
-		";
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 const int width = 800;
 const int height = 600;
@@ -107,11 +120,38 @@ int main()
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
+	//float vertices[] = {
+	//	-0.5f, -0.5f, 0.0f, // left
+	//	 0.5f, -0.5f, 0.0f, // right
+	//	 0.0f, 0.5f, 0.0f   // top
+	//};
+
+	//float vertices[] = {
+	//	// first triangle
+	//	 0.5f,  0.5f, 0.0f,	// top right
+	//	 0.5f, -0.5f, 0.0f,	// bottom right
+	//	-0.5f,  0.5f, 0.0f,	// top left
+	//	// second triangle
+	//	 0.5f, -0.5f, 0.0f,	// bottom right
+	//	-0.5f, -0.5f, 0.0f,	// bottom left
+	//	-0.5f,  0.5f, 0.0f	// top left
+	//};
+
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left
-		 0.5f, -0.5f, 0.0f, // right
-		 0.0f, 0.5f, 0.0f   // top
+		 0.5f,  0.5f, 0.0f,	// top right
+		 0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f,	// bottom left
+		-0.5f,  0.5f, 0.0f,	// top left
 	};
+
+	unsigned int indices[] = {
+		0, 1, 3,	// first triangle
+		1, 2, 3,		// second triangle
+	};
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
 
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -121,6 +161,9 @@ int main()
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -143,14 +186,7 @@ int main()
 	//// 3. now draw the object
 	//someOpenGLFunctionThatDrawsOurTriangle();
 
-
-
-
-	
-	
-
 	glViewport(0, 0, width, height);
-
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
@@ -166,7 +202,10 @@ int main()
 		// 4. draw the object
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
